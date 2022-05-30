52163e3bece3814ea541f79e736b88a475624877
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-30 10:42:05 -0300

    model Image configurada

--- socialMedia-laravel/app/Models/Image.php
+++ socialMedia-laravel/app/Models/Image.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $table = 'images';
+
+    // relacion one to many
+    public function comments(){
+        return $this->hasMany('App\Comment');
+    }
+
+    // relacion one to many
+    public function like(){
+        return $this->hasMany('App\Like');
+    }
+
+    // relacion many to one
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    
 }
