eb9449f2f4dd46d11a1450e38d5eee9522603356
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-30 10:49:59 -0300

    modificamos entidad user

--- socialMedia-laravel/app/Models/User.php
+++ socialMedia-laravel/app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // relacion one to many
+    public function images()
+    {
+        return $this->hasMany('App\Image');
+    }
 }
