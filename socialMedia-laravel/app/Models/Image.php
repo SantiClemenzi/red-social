a9445717c7b21cb939fc68abeb74684929219f19
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-30 10:46:08 -0300

    configuramos Comment model

--- socialMedia-laravel/app/Models/Image.php
+++ socialMedia-laravel/app/Models/Image.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
@@ -11,19 +11,20 @@ class Image extends Model
     protected $table = 'images';
 
     // relacion one to many
-    public function comments(){
+    public function comments()
+    {
         return $this->hasMany('App\Comment');
     }
 
     // relacion one to many
-    public function like(){
+    public function like()
+    {
         return $this->hasMany('App\Like');
     }
 
     // relacion many to one
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo('App\User', 'user_id');
     }
-
-    
 }
