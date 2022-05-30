8f29bafb6978252d744ffd3ff81bfb5ec7e38fe8
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-30 10:47:29 -0300

    configuramos entidad Like

--- socialMedia-laravel/app/Models/Like.php
+++ socialMedia-laravel/app/Models/Like.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $table = 'likes';
+
+    // relacion many to one
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    // relacion many to one
+    public function image()
+    {
+        return $this->belongsTo('App\Image', 'image_id');
+    }
 }
