db68937c72733763ddf270ee7ed5ce254ed3900f
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-31 11:06:13 -0300

    estilos solucionados

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::get('/', function () {
     die();
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
