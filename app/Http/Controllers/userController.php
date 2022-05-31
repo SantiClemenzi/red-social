bc815192aa74e656cf569814477a97790b6d6962
santiago clemenzi <clemenzisantiago@gmail.com>
2022-05-31 18:32:51 -0300

    realizamos la validacion de los datos

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class userController extends Controller
 {
@@ -15,6 +16,14 @@ class userController extends Controller
     public function update(Request $request)
     {
         $id = Auth::user()->id;
+
+        $validate = $this->validate($request,[
+            'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'username' => ['required', 'string', 'max:255', Rule::unique('users','username')->ignore($id, 'id')],
+            'email' => ['required', 'string', 'max:255', Rule::unique('users','email')->ignore($id, 'id')],
+        ]);
+
         $name = $request->input('name');
         $surname = $request->input('surname');
         $username = $request->input('username');
