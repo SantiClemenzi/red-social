19b2f5356d36ef15ec354517993f618be1c40328
santiago clemenzi <clemenzisantiago@gmail.com>
2022-06-04 18:55:47 -0300

    creamos validacion comment

--- app/Http/Controllers/commentController.php
+++ app/Http/Controllers/commentController.php
@@ -13,6 +13,11 @@ class commentController extends Controller
         }
         public function store(Request $request)
         {
+            $validate = $this->validate($request, [
+                'image_id' => 'integer|required',
+                'comment' => 'string|required',
+            ]);
+
             $image_id = $request->input('image_id');
             $comment = $request->input('comment');
             var_dump($image_id);
