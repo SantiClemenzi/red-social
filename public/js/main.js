7908c05baf75967632866545b07613bb059b49eb
santiago clemenzi <clemenzisantiago@gmail.com>
2022-06-06 16:19:10 -0300

    agregamos los like al detail

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,42 @@
 window.addEventListener("load", function () {
     $(".btn-like").css("cursor", "pointer");
     $(".btn-dislike").css("cursor", "pointer");
+
+    var url =
+        "http://localhost/projects/master_PHP/socialMedia-laravel/public/";
+
     $(document).on("click", ".btn-like", function (e) {
         $(this).addClass("btn-dislike").removeClass("btn-like");
         $(this).attr("fill", "red");
-        var url = 'http://localhost/projects/master_PHP/socialMedia-laravel/public/';
+
         $.ajax({
-            url: url + 'like/'+$(this).data('id'),
-            type: 'GET',
-            success: function(response){
-                console.log(response);
-                // if(response.like){
-                //     console.log('Has dado like a la publicacion');
-                // }else{
-                //     console.log('Error al dar like');
-                // }
+            url: url + "like/" + $(this).data("id"),
+            type: "GET",
+            success: function (response) {
+                // console.log(response);
+                if (response.like) {
+                    alert("Has dado like a la publicacion");
+                } else {
+                    console.log("Error al dar like");
+                }
             },
         });
     });
     $(document).on("click", ".btn-dislike", function (e) {
         $(this).addClass("btn-like").removeClass("btn-dislike");
         $(this).attr("fill", "gray");
+
+        $.ajax({
+            url: url + "dislike/" + $(this).data("id"),
+            type: "GET",
+            success: function (response) {
+                // console.log(response);
+                if (response.like) {
+                    alert("Has dado dislike a la publicacion");
+                } else {
+                    console.log("Error al dar dislike");
+                }
+            },
+        });
     });
 });
--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -21,11 +21,23 @@
                     <div class="card" style="width: 100%;">
                         <img src="{{ route('image.file', ['filename'=>$image->image_path]) }}" alt="" class="card-img-top">
                     </div>
-
-                    <div class="likes" style="margin: 2%;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <?php $user_like = false; ?>
+                    <div class="like" style="margin: 2%; display: inline-block; text-align: justify;">
+                        @foreach($image->like as $like)
+                        @if($like->user->id == Auth::user()->id)
+                        <?php $user_like = true; ?>
+                        @endif
+                        @endforeach
+                        @if($user_like)
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="red" class="btn-like" data-id="{{$image->id}}" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                         </svg>
+                        @else
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="gray" class="btn-like" data-id="{{$image->id}}" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                        </svg>
+                        @endif
+                        <p style="color: gray; float: rigth;"> {{count($image->like)}} </p>
                     </div>
 
                     <div class="description" style="padding: 1%;">
@@ -53,7 +65,7 @@
                                 <p>
                                     {{$comment->content}}
                                     @if(Auth::check() && ($comment->user_id == Auth::user()->id || $comment->image->user_id == Auth::user()->id))
-                                        <a href="{{url('/comment/delete')}}/{{$comment->id}}" class="btn btn-sm btn-danger" style="float: right; margin-right: 2%;">Eliminar</a>
+                                    <a href="{{url('/comment/delete')}}/{{$comment->id}}" class="btn btn-sm btn-danger" style="float: right; margin-right: 2%;">Eliminar</a>
                                     @endif
                                 </p>
                             </div>
