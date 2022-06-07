ae37312cbe2aae7e2bebce9d47df5fce7f852057
santiago clemenzi <clemenzisantiago@gmail.com>
2022-06-07 09:09:04 -0300

    implementamos el paginator

--- resources/views/like/likes.blade.php
+++ resources/views/like/likes.blade.php
@@ -5,10 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1>Tus likes</h1>
+            <hr>
             @foreach($likes as $like)
             @include('includes.image', ['image'=>$like->image])
             @endforeach
-
+            <!-- paginacion -->
+            <div class="card text-center">
+                <div class="text-center">
+                    <div class="card-body"> {{$likes->render()}}</div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
